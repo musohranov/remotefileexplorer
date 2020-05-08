fb3fc6f577607df440e9bdb3bde74d119fe79b2d
musohranov <musohranov@gmail.com>
2020-05-08 11:35:36 +0700

    Избавился от дублирования кода

--- Server/src/com/remotefileexplorer/transport/Response.java
+++ Server/src/com/remotefileexplorer/transport/Response.java
@@ -10,43 +10,44 @@ final class Response {
 
     /**
      * Создать ответ с типом "Ошибка".
-     * @param message Сообщение об ошибке.
+     * @param body Содержимое ответа.
      */
-    static String createError(final String message) {
-        JSONObject jsonData = new JSONObject();
-
-        jsonData.put("result_type", "error");
-        jsonData.put("result_body", message);
-
-        return jsonData.toString();
+    static String createError(final String body) {
+        return createResponse(null, false, body);
     }
 
     /**
      * Создать ответ с типом "Ошибка".
      * @param request Запрос.
-     * @param message Сообщение об ошибке.
+     * @param body Содержимое ответа.
      */
-    static String createError(final Request request, final String message) {
-        JSONObject jsonData = new JSONObject();
-
-        jsonData.put("request_id", request.id);
-        jsonData.put("result_type", "error");
-        jsonData.put("result_body", message);
-
-        return jsonData.toString();
+    static String createError(final Request request, final String body) {
+        return createResponse(request.id, false, body);
     }
 
     /**
      * Создать ответ с типом "Успех".
      * @param request Запрос.
-     * @param message Сообщение об ошибке.
+     * @param body Содержимое ответа.
      */
-    static String createOk(final Request request, final String message) {
+    static String createOk(final Request request, final String body) {
+        return createResponse(request.id, true, body);
+    }
+
+    /**
+     * Создать ответ.
+     * @param request_id Идентификатор запроса.
+     * @param success Флаг результата обработки.
+     * @param body Содержимое ответа.
+     */
+    private static String createResponse(String request_id, boolean success, String body) {
         JSONObject jsonData = new JSONObject();
 
-        jsonData.put("request_id", request.id);
-        jsonData.put("result_type", "ok");
-        jsonData.put("result_body", message);
+        if (request_id != null)
+            jsonData.put("request_id", request_id);
+
+        jsonData.put("result_type", success ? "ok" : "error");
+        jsonData.put("result_body", body);
 
         return jsonData.toString();
     }
