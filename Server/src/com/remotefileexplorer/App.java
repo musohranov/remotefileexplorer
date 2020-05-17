abcd2b503e8fedf63bb4c472a9658645b55f62bb
musohranov <musohranov@gmail.com>
2020-05-17 21:34:06 +0700

    Реализовал интерфейс командной строки для запуска сервера

--- Server/src/com/remotefileexplorer/App.java
+++ Server/src/com/remotefileexplorer/App.java
@@ -1,17 +1,13 @@
 package com.remotefileexplorer;
 
-import com.remotefileexplorer.transport.Server;
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.CommandLine;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.cli.*;
+import com.remotefileexplorer.transport.Server;
+
 
 /**
  * Сервер.
@@ -29,21 +25,21 @@ public final class App {
      *             - Порт web сервера.
      */
     public static void main(final String[] args) throws IOException, InterruptedException {
-        Logger.getGlobal().setLevel(Level.FINE);
-
         Settings settings;
         try {
             settings = new Settings(args);
         } catch (ParseException e) {
-            Logger.getGlobal().warning("Не корректная установка настроек! " + e.toString());
+            Settings.printHelpFormatter();
             return;
         }
 
         File workingDirectory = new File(settings.workingDirectory);
         if (!workingDirectory.isDirectory()) {
-            Logger.getGlobal().warning("Директория задана не корректно!");
+            Settings.printHelpFormatter();
             return;
         }
+
+        Logger.getGlobal().setLevel(Level.FINE);
 
         if (!workingDirectory.canWrite() && settings.writePermission) {
             Logger.getGlobal().warning("К рабочей директории нет доступа на запись!");
@@ -52,55 +48,76 @@ public final class App {
 
         Server.start(workingDirectory, settings.writePermission, settings.port);
     }
-
-    /**
-     * Настройки.
-     */
-    private static class Settings {
-        /**
-         * Рабочая директория.
-         */
-        final String workingDirectory;
-
-        /**
-         * Разрешение на запись. Поумолчанию не разрешено.
-         */
-        final boolean writePermission;
-
-        /**
-         * Порт web сервера. По умолчанию 8080.
-         */
-        final int port;
-
-        /**
-         * @param args Входные аргументы приложения.
-         */
-        Settings(final String[] args) throws ParseException {
-            Options options = new Options();
-
-            Option oDirectory = new Option("d", true, "Рабочая директория");
-            oDirectory.setRequired(true);
-            options.addOption(oDirectory);
-
-            Option oPermission = new Option("w", false, "Разрешение на запись");
-            options.addOption(oPermission);
-
-            Option oPort = new Option("p", true, "Порт web сервера");
-            oPort.setRequired(true);
-            options.addOption(oPort);
-
-            CommandLineParser parser = new BasicParser();
-            CommandLine line = parser.parse(options, args);
-
-            this.workingDirectory = line.getOptionValue("d");
-            this.writePermission = line.hasOption("w");
-
-            int port = 8080;
-            try {
-                port = Integer.parseInt(line.getOptionValue("p"));
-            } catch (NumberFormatException ignored) {}
-            this.port = port;
-        }
-    }
 }
 
+/**
+ * Настройки.
+ */
+class Settings {
+    /**
+     * Рабочая директория.
+     */
+    final String workingDirectory;
+    private static final String WORKING_DIRECTORY_OPTION = "d";
+
+    /**
+     * Разрешение на запись. Поумолчанию не разрешено.
+     */
+    final boolean writePermission;
+    private static final String WRITE_PERMISSION_OPTION = "w";
+
+    /**
+     * Порт web сервера.
+     */
+    final int port;
+    private static final String PORT_OPTION = "p";
+
+    /**
+     * @param args Входные аргументы командной строки приложения.
+     */
+    Settings(final String[] args) throws ParseException {
+        CommandLineParser parser = new BasicParser();
+        CommandLine line = parser.parse(Settings.getOptions(), args);
+
+        this.workingDirectory = line.getOptionValue(WORKING_DIRECTORY_OPTION);
+        this.writePermission = line.hasOption(WRITE_PERMISSION_OPTION);
+        this.port = Integer.parseInt(line.getOptionValue(PORT_OPTION));
+    }
+
+    /**
+     * Перечень опций парсера командной строки.
+     */
+    private static Options options = null;
+
+    /**
+     * Получить декларацию опций парсера командной строки.
+     */
+    private static Options getOptions() {
+        if (Settings.options == null) {
+            Settings.options = new Options();
+
+            OptionBuilder.hasArg().withArgName("Директория").isRequired()
+                    .withDescription("Рабочая директория относительно которой происходит выполнения команд");
+            Settings.options.addOption(OptionBuilder.create(WORKING_DIRECTORY_OPTION));
+
+            OptionBuilder.withDescription("Разрешение на запись");
+            Settings.options.addOption(OptionBuilder.create(WRITE_PERMISSION_OPTION));
+
+            OptionBuilder.hasArg().withArgName("Порт").isRequired()
+                    .withDescription("Порт web сервера");
+            Settings.options.addOption(OptionBuilder.create(PORT_OPTION));
+        }
+
+        return Settings.options;
+    }
+
+    /**
+     * Получить хелп по формату командной строки.
+     */
+    static void printHelpFormatter() {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.defaultSyntaxPrefix = "Использование: ";
+
+        helpFormatter.printHelp("RemoteFileExplorer.jar", "", options, "", true);
+    }
+}
